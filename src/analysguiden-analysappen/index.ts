d3a7cc776b9f220f27832f2fae9db25a9caff76b
niklashans <104555311+niklashans@users.noreply.github.com>
2023-02-20 14:30:53 +0100

    Fix omxsip

--- src/analysguiden-analysappen/index.ts
+++ src/analysguiden-analysappen/index.ts
@@ -1,12 +1,14 @@
+const list = [...document.querySelectorAll('.omsx_item')];
+const curUpdate = list[0].querySelector('.omxspi-text')?.innerHTML.split(';')[2];
+const preUpdate = list[1].querySelector('.omxspi-text')?.innerHTML.split(';')[2];
 const icon = document.querySelector('.omsx_icon');
 const number = document.querySelector('.omxspi-text')?.innerHTML.split(';')[2];
 
 const updateArrow = function () {
-  if (number < 0) {
+  if (curUpdate < preUpdate) {
     icon?.classList.add('is-negative');
-  } else if (number > 0) {
+  } else if (curUpdate > preUpdate) {
     icon?.classList.add('is-positive');
   }
 };
-
 updateArrow();
